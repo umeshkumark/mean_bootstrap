ef7bcf508156c67fc07bd3eb8e38b0afe63d00a7
Umesh Kumar <umeshkumar.kumar@gmail.com>
2014-08-30 01:35:29 +0530

    added placeholders for actual values

--- superadmin.js
+++ superadmin.js
@@ -1,6 +1,6 @@
 var defaultSuperAdminUser = {
-    "name" : "super admin",
-    "email" : "superadmin@meanbootstrap.com",
+    "name" : "<user_name>",
+    "email" : "<email_id>",
     "login_id" : "superadmin",
     "password" : "17c4520f6cfd1ab53d8745e84681eb49", // superadmin
     "creationDate" : ISODate("2014-08-24T18:22:11.497Z"),
